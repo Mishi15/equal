2da9eaa53143e8e06843083c6c55d24cc835fd4c
Mishi15 <122174407+Mishi15@users.noreply.github.com>
2023-05-23 00:06:49 +0530

    Create equal.cpp

---
+++ equal.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    cout << "Enter the number";
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << "The entered no. are equal";
+    }
+    else
+    {
+        cout << " they are not equal";
+    }
+}
